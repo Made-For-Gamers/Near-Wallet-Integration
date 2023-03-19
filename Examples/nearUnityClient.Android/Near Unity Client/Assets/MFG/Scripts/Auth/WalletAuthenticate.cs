aa79a577554412f62ed7a3fdb07d2a4557233456
JP van Loggerenberg <jploggies@gmail.com>
2023-03-19 13:38:12 +0100

    NEAR wallet connection for WebGL

--- Examples/nearUnityClient.Android/Near Unity Client/Assets/MFG/Scripts/Auth/WalletAuthenticate.cs
+++ Examples/nearUnityClient.Android/Near Unity Client/Assets/MFG/Scripts/Auth/WalletAuthenticate.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Runtime.InteropServices;
 using TMPro;
+using System;
+using System.Web;
 
 public class WalletAuthenticate : MonoBehaviour
 {
@@ -8,8 +10,7 @@ public class WalletAuthenticate : MonoBehaviour
 
     [DllImport("__Internal")]
     private static extern void authenticateWithNearWallet(string appKey, string contractName, string onSuccess, string onFailure);
-       
-
+ 
     public void AuthenticateTest()
     {
          authenticateWithNearWallet("MFG App", "test", "OnAuthenticationSuccess", "OnAuthenticationFailure");
@@ -20,7 +21,7 @@ public class WalletAuthenticate : MonoBehaviour
     [SerializeField] private TextMeshProUGUI txtAccountId;
 
     public void OnAuthenticationSuccess(string accountId)
-    {
+    { 
         txtAccountId.text = accountId;
     }
 
@@ -28,4 +29,22 @@ public class WalletAuthenticate : MonoBehaviour
     {
         txtAccountId.text = error;
     }
+
+    void Start()
+    {
+        string currentUrl = Application.absoluteURL;
+        var uri = new Uri(currentUrl);
+        var queryParams = HttpUtility.ParseQueryString(uri.Query);
+
+        if(queryParams["account_id"] != null && queryParams["all_keys"] != null)
+        {
+            var accountId = queryParams["account_id"];
+            var allKeys =queryParams["all_keys"];
+
+            txtAccountId.text = accountId;
+        } 
+     
+    }
+ 
+ 
 }
--- Examples/nearUnityClient.Android/Near Unity Client/Assets/Plugins/Near.jslib
+++ Examples/nearUnityClient.Android/Near Unity Client/Assets/Plugins/Near.jslib
@@ -1,10 +1,26 @@
-mergeInto(LibraryManager.library, 
-{
+mergeInto(LibraryManager.library, {
 
- authenticateWithNearWallet: async function (appKey, contractName, onSuccess, onFailure) 
-{
+authenticateWithNearWallet: async function (appKey, contractName, onSuccess, onFailure) 
+{  
+    var config = {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        walletUrl: 'https://wallet.near.org'
+    };
 
-MyFunc(appKey, contractName, onSuccess, onFailure);
-}
+    var nearConnection = await nearApi.connect(config);
+    
+    var walletConnection = new nearApi.WalletConnection(nearConnection, 'Made-For-Gamers');
+    
+    walletConnection.requestSignIn({
+        contractId: "",
+        methodNames: [], // optional
+        //successUrl: "REPLACE_ME://.com/success", // optional redirect URL on success
+        //failureUrl: "REPLACE_ME://.com/failure" // optional redirect URL on failure
+    });
+
+ 
+},
+ 
 
 });
